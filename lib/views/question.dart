197b50e52d4ffa4267506a067f8d180197eb6c36
mehdialamiidrissi <M.alamiidrissi@aui.ma>
2022-07-15 00:49:25 +0100

    created the page for questions

---
+++ lib/views/question.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Question extends StatefulWidget {
+  const Question({Key? key}) : super(key: key);
+
+  @override
+  State<Question> createState() => _QuestionState();
+}
+
+class _QuestionState extends State<Question> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Stack(
+       children: <Widget>[
+           Container(color: Colors.blue[700]),
+            Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    
+                    },
+                  ),
+                ),
+            
+                
+                
+           Column(
+              
+              children: <Widget>[
+                SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Center(
+                    child: 
+                      Text(
+                        'What is the capital of Morocco?',
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                //4 cards for the options
+                Container(
+                  margin: EdgeInsets.only(left: 95, right: 95),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        child: Card(
+                          color: Colors.white,
+                          
+                          child: ListTile(
+                            title: Text(
+                              'Rabat',
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue[700]),
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Question()),
+                              );
+                            },
+                          ),
+                        ),
+                        
+                      ),
+                      Container(
+                        child: Card(
+                          color: Colors.white,
+                          
+                          child: ListTile(
+                            title: Text(
+                              'Casablanca',
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue[700]),
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Question()),
+                              );
+                            },
+                          ),
+                        ),
+                        
+                        
+                      ),
+                      Container(
+                        child: Card(
+                          color: Colors.white,
+                          
+                          child: ListTile(
+                            title: Text(
+                              'Marrakech',
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue[700]),
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Question()),
+                              );
+                            },
+                          ),
+                        ),
+                        
+                      ),
+                      Container(
+                        child: Card(
+                          color: Colors.white,
+                          
+                          child: ListTile(
+                            title: Text(
+                              'Ifrane',
+                              style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue[700]),
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => Question()),
+                              );
+                            },
+                          ),
+                        ),
+                        
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+           
+
+           
+       ],
+        
+        
+        
+      )
+    );
+    
+  }
+}
--- lib/views/subjects.dart
+++ lib/views/subjects.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:triviamaster/views/question.dart';
 
 class Subjects extends StatefulWidget {
   const Subjects({Key? key}) : super(key: key);
@@ -14,158 +15,150 @@ class _SubjectsState extends State<Subjects> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        
         children: <Widget>[
           Container(color: Colors.blue[700]),
-          
-        
-       
-          
           SingleChildScrollView(
-            
-            child: Stack(
-              children: <Widget>[ 
-                Container(
-                  margin: EdgeInsets.only(top: 20),
-                  child:IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
-              onPressed: () {
-               
-                Navigator.pop(context);
-              },
-            ),
-
+            child: Stack(children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
-                 
-                Column(
-                  
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(top: 100),
-                      child: Text(
-                        'Subjects',
-                        style: TextStyle(
-                            fontSize: 40.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
+              ),
+              Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 100),
+                    child: Text(
+                      'Subjects',
+                      style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Text(
+                      'Select a subject to start',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 95, left: 95),
+                    child: Card(
+                      child: ListTile(
+                        title: Text('Science'),
+                        //add icon
+                        leading: Icon(Icons.science),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Question()),
+                          );
+                        },
                       ),
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: 20),
-                      child: Text(
-                        'Select a subject to start',
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 95, left: 95),
+                    child: Card(
+                      child: ListTile(
+                        title: Text('Music'),
+                        //add icon
+                        leading: Icon(Icons.music_note),
+                        onTap: () {},
                       ),
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 95, left: 95),
-                      child: Card(
-                        child: ListTile(
-                          title: Text('Science'),
-                          //add icon
-                          leading: Icon(Icons.science),
-                          onTap: () {},
-                        ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 95, left: 95),
+                    child: Card(
+                      child: ListTile(
+                        title: Text('Film & TV'),
+                        //add icon
+                        leading: Icon(Icons.tv),
+                        onTap: () {},
                       ),
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 95, left: 95),
-                      child: Card(
-                        child: ListTile(
-                          title: Text('Music'),
-                          //add icon
-                          leading: Icon(Icons.music_note),
-                          onTap: () {},
-                        ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 95, left: 95),
+                    child: Card(
+                      child: ListTile(
+                        title: Text('History'),
+                        //add icon
+                        leading: Icon(Icons.history_edu),
+                        onTap: () {},
                       ),
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 95, left: 95),
-                      child: Card(
-                        child: ListTile(
-                          title: Text('Film & TV'),
-                          //add icon
-                          leading: Icon(Icons.tv),
-                          onTap: () {},
-                        ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 95, left: 95),
+                    child: Card(
+                      child: ListTile(
+                        title: Text('Geography'),
+                        //add icon
+                        leading: Icon(Icons.location_on),
+                        onTap: () {},
                       ),
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 95, left: 95),
-                      child: Card(
-                        child: ListTile(
-                          title: Text('History'),
-                          //add icon
-                          leading: Icon(Icons.history_edu),
-                          onTap: () {},
-                        ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 95, left: 95),
+                    child: Card(
+                      child: ListTile(
+                        title: Text('Sport & Leisure'),
+                        //add icon
+                        leading: Icon(Icons.sports_football),
+                        onTap: () {},
                       ),
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 95, left: 95),
-                      child: Card(
-                        child: ListTile(
-                          title: Text('Geography'),
-                          //add icon
-                          leading: Icon(Icons.location_on),
-                          onTap: () {},
-                        ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 95, left: 95),
+                    child: Card(
+                      child: ListTile(
+                        title: Text('General Knowledge'),
+                        //add icon
+                        leading: Icon(Icons.public),
+                        onTap: () {},
                       ),
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 95, left: 95),
-                      child: Card(
-                        child: ListTile(
-                          title: Text('Sport & Leisure'),
-                          //add icon
-                          leading: Icon(Icons.sports_football),
-                          onTap: () {},
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(right: 95, left: 95),
-                      child: Card(
-                        child: ListTile(
-                          title: Text('General Knowledge'),
-                          //add icon
-                          leading: Icon(Icons.public),
-                          onTap: () {},
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ]
-              
-            ),
+                  ),
+                ],
+              ),
+            ]),
           ),
         ],
       ),
